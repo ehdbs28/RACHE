ebc9fa5a92a95003ed2a127834af8ee995b262e2
ehdbs28 <doyun5373@naver.com>
2022-06-21 09:12:37 +0900

    BGMMixer & 오디오세팅과 연결

--- Assets/01.Scripts/SettingMenu.cs
+++ Assets/01.Scripts/SettingMenu.cs
@@ -10,40 +10,23 @@ public class SettingMenu : BtnManager
     [SerializeField] TextMeshProUGUI _bgmSoundTxt;
     [SerializeField] AudioMixer masterMixer;
 
-    private int _effectSound = 100;
-    private int _bgmSound = 100;
+    private float _effectSound = 1;
+    private float _bgmSound = 1;
     private void Update()
     {
         BtnMove();
-        
-        if (Input.GetKeyDown(KeyCode.Return))
-        {
-            if(_currentBtnNum == 0)
-            {
-                StageManager.Instance.IsSetting = false;
-                StageManager.Instance.IsESC = true;
-            }
-        }
-        masterMixer.SetFloat("BGM", Mathf.Lerp(-40, 0, _bgmSound));
-
-        _effectSoundTxt.text = $"효과음 : {_effectSound}";
-        _bgmSoundTxt.text = $"배경음 : {_bgmSound}";
-    }
-
-    private void LateUpdate()
-    {
         #region 볼륨 업
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
             if (_currentBtnNum == 2)
             {
-                _effectSound++;
-                _effectSound = Mathf.Clamp(_effectSound, 0, 100);
+                _effectSound += 0.001f;
+                _effectSound = Mathf.Clamp(_effectSound, 0, 1);
             }
             if (_currentBtnNum == 1)
             {
-                _bgmSound++;
-                _bgmSound = Mathf.Clamp(_bgmSound, 0, 100);
+                _bgmSound += 0.001f;
+                _bgmSound = Mathf.Clamp(_bgmSound, 0, 1);
             }
         }
         #endregion
@@ -52,15 +35,31 @@ public class SettingMenu : BtnManager
         {
             if (_currentBtnNum == 2)
             {
-                _effectSound--;
-                _effectSound = Mathf.Clamp(_effectSound, 0, 100);
+                _effectSound -= 0.001f;
+                _effectSound = Mathf.Clamp(_effectSound, 0, 1);
             }
             if (_currentBtnNum == 1)
             {
-                _bgmSound--;
-                _bgmSound = Mathf.Clamp(_bgmSound, 0, 100);
+                _bgmSound -= 0.001f;
+                _bgmSound = Mathf.Clamp(_bgmSound, 0, 1);
             }
         }
         #endregion
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if(_currentBtnNum == 0)
+            {
+                StageManager.Instance.IsSetting = false;
+                StageManager.Instance.IsESC = true;
+            }
+        }
+        if (_bgmSound == 0) { masterMixer.SetFloat("BGM", -80); }
+        else { masterMixer.SetFloat("BGM", Mathf.Lerp(-40, 0, _bgmSound)); }
+
+        if (_effectSound == 0) { masterMixer.SetFloat("Effect", -80); }
+        else { masterMixer.SetFloat("Effect", Mathf.Lerp(-40, 0, _effectSound)); }
+
+        _effectSoundTxt.text = $"효과음 : {System.Math.Round(_effectSound * 100, 1)}";
+        _bgmSoundTxt.text = $"배경음 : {System.Math.Round(_bgmSound * 100, 1)}";
     }
 }
