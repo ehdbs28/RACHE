f25f5b3ee5034bc40ea6d9bb27b4921d18fa2fcb
ehdbs28 <doyun5373@naver.com>
2022-06-25 15:38:10 +0900

    오디오세팅 개편

--- Assets/01.Scripts/SettingMenu.cs
+++ Assets/01.Scripts/SettingMenu.cs
@@ -10,38 +10,38 @@ public class SettingMenu : BtnManager
     [SerializeField] TextMeshProUGUI _bgmSoundTxt;
     [SerializeField] AudioMixer masterMixer;
 
-    private float _effectSound = 1;
-    private float _bgmSound = 1;
+    private float _effectSound = 10;
+    private float _bgmSound = 10;
     private void Update()
     {
         BtnMove();
         #region 볼륨 업
-        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
         {
             if (_currentBtnNum == 2)
             {
-                _effectSound += 0.001f;
-                _effectSound = Mathf.Clamp(_effectSound, 0, 1);
+                _effectSound++;
+                _effectSound = Mathf.Clamp(_effectSound, 0, 10);
             }
             if (_currentBtnNum == 1)
             {
-                _bgmSound += 0.001f;
-                _bgmSound = Mathf.Clamp(_bgmSound, 0, 1);
+                _bgmSound++;
+                _bgmSound = Mathf.Clamp(_bgmSound, 0, 10);
             }
         }
         #endregion
         #region 볼륨 다운
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
         {
             if (_currentBtnNum == 2)
             {
-                _effectSound -= 0.001f;
-                _effectSound = Mathf.Clamp(_effectSound, 0, 1);
+                _effectSound--;
+                _effectSound = Mathf.Clamp(_effectSound, 0, 10);
             }
             if (_currentBtnNum == 1)
             {
-                _bgmSound -= 0.001f;
-                _bgmSound = Mathf.Clamp(_bgmSound, 0, 1);
+                _bgmSound--;
+                _bgmSound = Mathf.Clamp(_bgmSound, 0, 10);
             }
         }
         #endregion
@@ -54,12 +54,12 @@ public class SettingMenu : BtnManager
             }
         }
         if (_bgmSound == 0) { masterMixer.SetFloat("BGM", -80); }
-        else { masterMixer.SetFloat("BGM", Mathf.Lerp(-40, 0, _bgmSound)); }
+        else { masterMixer.SetFloat("BGM", Mathf.Lerp(-40, 0, _bgmSound / 10)); }
 
         if (_effectSound == 0) { masterMixer.SetFloat("Effect", -80); }
-        else { masterMixer.SetFloat("Effect", Mathf.Lerp(-40, 0, _effectSound)); }
+        else { masterMixer.SetFloat("Effect", Mathf.Lerp(-40, 0, _effectSound / 10)); }
 
-        _effectSoundTxt.text = $"효과음 : {System.Math.Round(_effectSound * 100, 1)}";
-        _bgmSoundTxt.text = $"배경음 : {System.Math.Round(_bgmSound * 100, 1)}";
+        _effectSoundTxt.text = $"효과음 : {_effectSound}";
+        _bgmSoundTxt.text = $"배경음 : {_bgmSound}";
     }
 }
