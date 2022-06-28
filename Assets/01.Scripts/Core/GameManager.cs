0d8aaef5bc2e3e8c0efe0bebfc3770df9f62c979
ehdbs28 <doyun5373@naver.com>
2022-06-28 15:34:54 +0900

    로고추가 && 리스타트 추가

--- Assets/01.Scripts/Core/GameManager.cs
+++ Assets/01.Scripts/Core/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -33,5 +34,13 @@ public class GameManager : MonoBehaviour
 
         ScoreManager.Instance = gameObject.AddComponent<ScoreManager>();
         #endregion
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene("GamePlay");
+        }
     }
 }
